64ebfe9bee4ebfe0348003c968f95045a1aedf1a
tranducthien28 <tranthien242000@gmail.com>
2022-10-19 10:38:52 +0700

    up code

--- js/main.js
+++ js/main.js
@@ -14,18 +14,33 @@ $(function(){
         1000:{items: 5}
     } 
 })
+$('._news-slide').owlCarousel({loop: 1, items: 4, margin: 25, dots: 1, nav: 1, navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+responsive:{
+    0:{items: 2},
+    600:{items: 4},
+    1000:{items: 4}
+} 
+})
+
+$('.leader-cnt').owlCarousel({loop: true, margin: 70, items: 4, dots: 1, nav: 1, navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']});
+$('.success-cnt').owlCarousel({loop: true, margin: 70, items: 3, dots: 1, nav: 1, navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']});
 
     const resizeImage = (e, s) => { s = parseInt($(e).width()) * s; $(e).css({ height: s + "px" }) };
     resizeImage('.slider .img',555/1365);
     resizeImage('.ab-cnt .img', 360/575);
     resizeImage('.field-item .item .img', 475/385)
     resizeImage('.pro-cnt .item .img', 1);
-    resizeImage('.news-cnt .left .img', 385/592);
-    resizeImage('.news-cnt .right .item .img', 181/282);
+    resizeImage('.news-cnt .left.news-item .img', 385/592);    
     resizeImage('.cus-cnt .item',165/230);
     resizeImage('.banner .img', 430/1365);
     resizeImage('._ab-cnt .img',340/585);
-    resizeImage('.core-cnt .it', 260/370)
+    resizeImage('.core-cnt .it', 260/370);
+    resizeImage('._field-item .img', 345/575);
+    resizeImage('._news-top .img', 380/590);
+    resizeImage('.news-item .img', 181/282);
+    resizeImage('.right .news-item .img', 181/282);
+    // resizeImage('.core-cnt .item', 370/208)
+
     if($(window).width() < 767){
         resizeImage('.slider .img',9/16);
     }
